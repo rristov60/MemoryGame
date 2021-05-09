c6b843d4bc913456066c0ca7193126ad655e064e
unknown <risteristov60@yahoo.com>
2021-05-09 13:53:59 +0200

    Added loading screen

---
+++ StartUpScreen/Loader.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace StartUpScreen
+{
+    partial class Loader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.credits = new System.Windows.Forms.Label();
+            this.timerLoadingScreen = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StartUpScreen.Properties.Resources.loaderFinal;
+            this.pictureBox1.Location = new System.Drawing.Point(248, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 228);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Futura Bk BT", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(176, 270);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(447, 56);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "THE MIND TWISTER";
+            // 
+            // credits
+            // 
+            this.credits.AutoSize = true;
+            this.credits.Font = new System.Drawing.Font("Futura Bk BT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.credits.Location = new System.Drawing.Point(258, 338);
+            this.credits.Name = "credits";
+            this.credits.Size = new System.Drawing.Size(284, 18);
+            this.credits.TabIndex = 2;
+            this.credits.Text = "A Memory Game created by Riste Ristov©";
+            // 
+            // timerLoadingScreen
+            // 
+            this.timerLoadingScreen.Interval = 10000;
+            this.timerLoadingScreen.Tick += new System.EventHandler(this.timerLoadingScreen_Tick);
+            // 
+            // Loader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 407);
+            this.Controls.Add(this.credits);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Loader";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label credits;
+        private System.Windows.Forms.Timer timerLoadingScreen;
+    }
+}
+
---
+++ StartUpScreen/Loader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LogInScreen;
+namespace StartUpScreen
+{
+    public partial class Loader : Form
+    {
+        public Loader()
+        {
+            InitializeComponent();
+            timerLoadingScreen.Start();
+        }
+
+        private void timerLoadingScreen_Tick(object sender, EventArgs e)
+        {
+            timerLoadingScreen.Stop();
+            LogInForm L = new LogInForm();
+            this.Hide();
+            L.Show();
+        }
+    }
+}
